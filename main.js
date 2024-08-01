2e75a2d9230b7041662839eee3191df8357e0818
MICHELL CHAVEZ FLORES <michell.chf27@gmail.com>
2024-07-31 21:04:28 -0600

    agregue una nueva extensión con Java script

---
+++ main.js
@@ -0,0 +1,15 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('<h2>Desarrolladora Fronted Jr. | Programadora</h2>')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Si lo puedes imaginar lo puedes crear')
+    .pauseFor(2500)
+    .deleteChars(5)
+    .typeString('<strong>programar</strong>')
+    .pauseFor(2500)
+    .start();
